371f01c327f25160114ba114f6136e2092b3cb65
David <David@LAPTOP-VSEKM5I2>
2018-04-12 18:51:06 +0100

    Commit git.

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,19 @@
+
+package boletin24;
+
+import java.awt.EventQueue;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Eventos obx=new Eventos();
+    }
+    
+}
